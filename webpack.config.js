430ed130ec7b0e133e9cb6c097d1b6edd7b908aa
nevadimka <morozovvadimka@gmail.com>
2022-08-20 20:18:56 +0300

    E12.6 changes - minificators - saved

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: "bundle.js"
     },
+    mode: 'development',
     plugins: [
         new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.pug',
             filename: 'index.html',
         }),
+        new TerserWebpackPlugin(),
+        new CssMinimizerWebpackPlugin(),
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserWebpackPlugin(), new CssMinimizerWebpackPlugin()]
+    },
     module: {
         rules: [
             {
@@ -27,7 +36,7 @@ module.exports = {
             },
             {
                 use: 'pug-loader',
-                test: /\.pug$/, 
+                test: /\.pug$/,
             }
         ]
     },
